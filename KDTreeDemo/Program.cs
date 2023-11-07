156e2aa52f109be1860011fa6c6fc8792a557883
thecsharper <si@thecsharper.com>
2023-11-07 18:40:55 +0000

    Add project files.

---
+++ KDTreeDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using KdTree.Math;
+using KdTree;
+
+var tree = new KdTree<float, int>(2, new FloatMath())
+{
+    { new[] { 50.0f, 80.0f }, 100 },
+    { new[] { 20.0f, 10.0f }, 200 }
+};
+
+var nodes = tree.GetNearestNeighbours(new[] { 30.0f, 20.0f }, 1);
+
+
+foreach (var node in nodes)
+{
+    Console.WriteLine($"{node.Point.FirstOrDefault()} {node.Value}");
+}
+
